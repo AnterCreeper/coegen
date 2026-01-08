08bd7bbb6b06c822c885af151812c32fad05d4d5
Zhihao Wang <wangzhihao9@hotmail.com>
2026-01-08 20:54:16 +0800

    add GOWIN support.

--- coegen.c
+++ coegen.c
@@ -3,7 +3,16 @@
 #include <string.h>
 #include <getopt.h>
 
-#define OPT_STRING "b:o:"
+#define OPT_STRING 		"b:o:"
+#define DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
+
+size_t fsize(FILE* fd) {
+	size_t pos = ftell(fd);
+	fseek(fd, 0, SEEK_END);
+	size_t result = ftell(fd);
+	fseek(fd, pos, SEEK_SET);
+	return result;
+}
 
 int main(int argc, char* argv[]) {
 	int size = 0;
@@ -30,8 +39,15 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "%s: error: no input files\n", argv[0]);
 		return -1;
 	}
+
 	FILE* src = fopen(argv[optind], "rb");
-	fprintf(result, "memory_initialization_radix=16;\nmemory_initialization_vector=\n");
+	if(!src) {
+		fprintf(stderr, "%s: error: no such file or directory\n", argv[0]);
+		return -1;
+	}
+
+	//fprintf(result, "memory_initialization_radix=16;\nmemory_initialization_vector=\n"); //for Xilinx
+	fprintf(result, "#File_format=Hex\n#Address_depth=%d\n#Data_width=%d\n", DIV_ROUND_UP(fsize(src),size), size*8);	//for GOWIN
 
 	for (;;) {
 		unsigned char buf[64];
@@ -40,7 +56,7 @@ int main(int argc, char* argv[]) {
 		for (int i = size - 1; i >= 0; i--)
 			fprintf(result, "%02x", buf[i]);
 		if (feof(src)) {
-			fprintf(result, ";\n");
+			//fprintf(result, ";\n"); //for Xilinx
 			break;
 		}
 		fprintf(result, "\n");
